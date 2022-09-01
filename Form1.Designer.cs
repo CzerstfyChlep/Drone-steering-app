988896b693b6ed6915a34f1c946e32144d0defda
CzerstfyChlep <35424664+CzerstfyChlep@users.noreply.github.com>
2022-09-01 17:56:20 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace RecieveStream
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.OldControlsRadio = new System.Windows.Forms.RadioButton();
+            this.NewControlsRadio = new System.Windows.Forms.RadioButton();
+            this.RestartConnectionButton = new System.Windows.Forms.Button();
+            this.InfoLabel = new System.Windows.Forms.Label();
+            this.OffOnDrone = new System.Windows.Forms.Label();
+            this.LatencyDrone = new System.Windows.Forms.Label();
+            this.LatencyInfoLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.Location = new System.Drawing.Point(12, 11);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(753, 433);
+            this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture.TabIndex = 1;
+            this.picture.TabStop = false;
+            // 
+            // OldControlsRadio
+            // 
+            this.OldControlsRadio.AutoSize = true;
+            this.OldControlsRadio.BackColor = System.Drawing.Color.Transparent;
+            this.OldControlsRadio.Checked = true;
+            this.OldControlsRadio.ForeColor = System.Drawing.Color.Snow;
+            this.OldControlsRadio.Location = new System.Drawing.Point(22, 453);
+            this.OldControlsRadio.Name = "OldControlsRadio";
+            this.OldControlsRadio.Size = new System.Drawing.Size(163, 17);
+            this.OldControlsRadio.TabIndex = 2;
+            this.OldControlsRadio.TabStop = true;
+            this.OldControlsRadio.Text = "Old controls (W/S Up/Down)";
+            this.OldControlsRadio.UseVisualStyleBackColor = false;
+            this.OldControlsRadio.CheckedChanged += new System.EventHandler(this.ChangeControlScheme);
+            // 
+            // NewControlsRadio
+            // 
+            this.NewControlsRadio.AutoSize = true;
+            this.NewControlsRadio.BackColor = System.Drawing.Color.Transparent;
+            this.NewControlsRadio.ForeColor = System.Drawing.Color.Snow;
+            this.NewControlsRadio.Location = new System.Drawing.Point(189, 453);
+            this.NewControlsRadio.Name = "NewControlsRadio";
+            this.NewControlsRadio.Size = new System.Drawing.Size(128, 17);
+            this.NewControlsRadio.TabIndex = 3;
+            this.NewControlsRadio.Text = "New controls (Arrows)";
+            this.NewControlsRadio.UseVisualStyleBackColor = false;
+            this.NewControlsRadio.CheckedChanged += new System.EventHandler(this.ChangeControlScheme);
+            // 
+            // RestartConnectionButton
+            // 
+            this.RestartConnectionButton.Location = new System.Drawing.Point(320, 450);
+            this.RestartConnectionButton.Name = "RestartConnectionButton";
+            this.RestartConnectionButton.Size = new System.Drawing.Size(184, 23);
+            this.RestartConnectionButton.TabIndex = 0;
+            this.RestartConnectionButton.Text = "Establish connection";
+            this.RestartConnectionButton.UseVisualStyleBackColor = true;
+            this.RestartConnectionButton.Click += new System.EventHandler(this.RestartConnectionButton_Click);
+            // 
+            // InfoLabel
+            // 
+            this.InfoLabel.AutoSize = true;
+            this.InfoLabel.ForeColor = System.Drawing.Color.Snow;
+            this.InfoLabel.Location = new System.Drawing.Point(518, 455);
+            this.InfoLabel.Name = "InfoLabel";
+            this.InfoLabel.Size = new System.Drawing.Size(70, 13);
+            this.InfoLabel.TabIndex = 5;
+            this.InfoLabel.Text = "Drone status:";
+            // 
+            // OffOnDrone
+            // 
+            this.OffOnDrone.AutoSize = true;
+            this.OffOnDrone.ForeColor = System.Drawing.Color.Lime;
+            this.OffOnDrone.Location = new System.Drawing.Point(594, 455);
+            this.OffOnDrone.Name = "OffOnDrone";
+            this.OffOnDrone.Size = new System.Drawing.Size(37, 13);
+            this.OffOnDrone.TabIndex = 6;
+            this.OffOnDrone.Text = "Offline";
+            // 
+            // LatencyDrone
+            // 
+            this.LatencyDrone.AutoSize = true;
+            this.LatencyDrone.ForeColor = System.Drawing.Color.Lime;
+            this.LatencyDrone.Location = new System.Drawing.Point(713, 455);
+            this.LatencyDrone.Name = "LatencyDrone";
+            this.LatencyDrone.Size = new System.Drawing.Size(26, 13);
+            this.LatencyDrone.TabIndex = 8;
+            this.LatencyDrone.Text = "0ms";
+            // 
+            // LatencyInfoLabel
+            // 
+            this.LatencyInfoLabel.AutoSize = true;
+            this.LatencyInfoLabel.ForeColor = System.Drawing.Color.Snow;
+            this.LatencyInfoLabel.Location = new System.Drawing.Point(637, 455);
+            this.LatencyInfoLabel.Name = "LatencyInfoLabel";
+            this.LatencyInfoLabel.Size = new System.Drawing.Size(76, 13);
+            this.LatencyInfoLabel.TabIndex = 7;
+            this.LatencyInfoLabel.Text = "Drone latency:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(777, 492);
+            this.Controls.Add(this.RestartConnectionButton);
+            this.Controls.Add(this.OldControlsRadio);
+            this.Controls.Add(this.LatencyDrone);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.NewControlsRadio);
+            this.Controls.Add(this.LatencyInfoLabel);
+            this.Controls.Add(this.OffOnDrone);
+            this.Controls.Add(this.InfoLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Drone";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.RadioButton OldControlsRadio;
+        private System.Windows.Forms.RadioButton NewControlsRadio;
+        private System.Windows.Forms.Button RestartConnectionButton;
+        private System.Windows.Forms.Label InfoLabel;
+        private System.Windows.Forms.Label OffOnDrone;
+        private System.Windows.Forms.Label LatencyDrone;
+        private System.Windows.Forms.Label LatencyInfoLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace RecieveStream
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (Control control in this.Controls)
+            {
+                control.PreviewKeyDown += new PreviewKeyDownEventHandler(control_PreviewKeyDown);
+            }
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            ControlConnection.StartSocket();
+
+            CheckConnectionThread = new Thread(CheckConnection);
+            CheckConnectionThread.Start();
+
+            ImageThread = new Thread(WirelessNetworkConnect);
+            ImageDownloaded += UpdateImg;
+            ImageThread.Start();
+
+            KeyDown += KeyDownV;
+            KeyUp += KeyUpV;          
+
+            FormClosing += Close;
+
+        }
+
+        void Close(object sender, EventArgs e)
+        {
+            if(ImageThread != null)
+                ImageThread.Abort();
+            if(ControlThread != null)
+                ControlThread.Abort();
+            if (CheckConnectionThread != null)
+                CheckConnectionThread.Abort();
+            stop = true;
+        }
+
+
+        public Thread ControlThread;
+        public Thread ImageThread;
+        public Thread CheckConnectionThread;
+
+        public delegate void UpdateImage();
+        public event UpdateImage ImageDownloaded;
+
+        public List<byte[]> ByteChunks = new List<byte[]>();
+
+
+        private int LeftMotorValue = 0;
+        public int LeftMotor
+        {            
+            get
+            {
+                return LeftMotorValue;
+            }
+            set
+            {
+                int oldValue = LeftMotorValue;
+                LeftMotorValue = value;
+                if (oldValue != 0 && LeftMotorValue == 0)
+                    ControlConnection.SendMessage("s1");
+                else if (oldValue == 0 && LeftMotorValue > 0)
+                    ControlConnection.SendMessage("f1");
+                else if (oldValue == 0 && LeftMotorValue < 0)
+                    ControlConnection.SendMessage("b1");
+
+            }
+        }
+        private int RightMotorValue = 0;
+        public int RightMotor
+        {
+            get
+            {
+                return RightMotorValue;
+            }
+            set
+            {
+                int oldValue = RightMotorValue;
+                RightMotorValue = value;
+                if (oldValue != 0 && RightMotorValue == 0)
+                    ControlConnection.SendMessage("s2");
+                else if (oldValue == 0 && RightMotorValue > 0)
+                    ControlConnection.SendMessage("f2");
+                else if (oldValue == 0 && RightMotorValue < 0)
+                    ControlConnection.SendMessage("b2");
+            }
+        }
+
+        public byte[] ImageByteArray = new byte[] { };
+
+        void control_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            e.IsInputKey = true;
+            if (!KeysPressed.Contains(e.KeyCode))
+            {
+                KeysPressed.Add(e.KeyCode);
+                if (OldControlsRadio.Checked)
+                {
+                    if (e.KeyCode == Keys.Escape)
+                        stop = true;
+                    else if (e.KeyCode == Keys.W)
+                    {
+                        RightMotor += 1;
+                    }
+                    else if (e.KeyCode == Keys.S)
+                    {
+                        RightMotor -= 1;
+                    }
+                    else if (e.KeyCode == Keys.Up)
+                    {
+                        LeftMotor += 1;
+                    }
+                    else if (e.KeyCode == Keys.Down)
+                    {
+                        LeftMotor -= 1;
+                    }
+                }
+                else if (NewControlsRadio.Checked)
+                {
+                    if (e.KeyCode == Keys.Up)
+                    {
+                        LeftMotor += 1;
+                        RightMotor += 1;
+                    }
+                    else if (e.KeyCode == Keys.Down)
+                    {
+                        LeftMotor -= 1;
+                        RightMotor -= 1;
+                    }
+                    else if (e.KeyCode == Keys.Right)
+                    {
+                        RightMotor += 1;
+                    }
+                    else if (e.KeyCode == Keys.Left)
+                    {
+                        LeftMotor += 1;
+                    }
+                    else if (e.KeyCode == Keys.Oemcomma)
+                    {
+                        LeftMotor += 1;
+                        RightMotor -= 1;
+                    }
+                    else if (e.KeyCode == Keys.OemPeriod)
+                    {
+                        LeftMotor -= 1;
+                        RightMotor += 1;
+                    }
+                }
+            }
+            if (KeysPressed.Any())
+            {
+                OldControlsRadio.Enabled = false;
+                NewControlsRadio.Enabled = false;
+            }
+            else
+            {
+                OldControlsRadio.Enabled = true;
+                NewControlsRadio.Enabled = true;
+            }
+        }
+
+        public void KeyDownV(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        public void KeyUpV(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            if (KeysPressed.Contains(e.KeyCode))
+            {
+                KeysPressed.Remove(e.KeyCode);
+                if (OldControlsRadio.Checked)
+                {
+                    if (e.KeyCode == Keys.Escape)
+                        stop = true;
+                    else if (e.KeyCode == Keys.W)
+                    {
+                        RightMotor -= 1;
+                    }
+                    else if (e.KeyCode == Keys.S)
+                    {
+                        RightMotor += 1;
+                    }
+                    else if (e.KeyCode == Keys.Up)
+                    {
+                        LeftMotor -= 1;
+                    }
+                    else if (e.KeyCode == Keys.Down)
+                    {
+                        LeftMotor += 1;
+                    }
+                }
+                else if (NewControlsRadio.Checked)
+                {
+                    if (e.KeyCode == Keys.Up)
+                    {
+                        LeftMotor -= 1;
+                        RightMotor -= 1;
+                    }
+                    else if (e.KeyCode == Keys.Down)
+                    {
+                        LeftMotor += 1;
+                        RightMotor += 1;
+                    }
+                    else if (e.KeyCode == Keys.Left)
+                    {
+                        LeftMotor -= 1;
+                    }
+                    else if (e.KeyCode == Keys.Right)
+                    {
+                        RightMotor -= 1;
+                    }
+                    else if (e.KeyCode == Keys.Oemcomma)
+                    {
+                        LeftMotor -= 1;
+                        RightMotor += 1;
+                    }
+                    else if (e.KeyCode == Keys.OemPeriod)
+                    {
+                        LeftMotor += 1;
+                        RightMotor -= 1;
+                    }
+                }
+            }
+            if (KeysPressed.Any())
+            {
+                OldControlsRadio.Enabled = false;
+                NewControlsRadio.Enabled = false;
+            }
+            else
+            {
+                OldControlsRadio.Enabled = true;
+                NewControlsRadio.Enabled = true;
+            }
+        }
+
+        bool stop = false;
+        public List<string> requests = new List<string>();
+
+        public void HandleRequestFunction()
+        {
+            if (requests.Any())
+            {
+                ControlConnection.SendMessage(requests.First());
+                requests.Remove(requests.First());
+            }
+        }  
+
+        public List<Keys> KeysPressed = new List<Keys>();
+
+        public void WirelessNetworkConnect()
+        {
+            UdpClient con = new UdpClient(50001);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 50001);
+            while (!stop)
+            {
+                recieveMessage(con, groupEP);
+            }
+        }
+
+        public void CheckConnection()
+        {
+            while (!stop)
+            {
+                if (ControlConnection.Client.Connected)
+                {
+                    OffOnDrone.Text = "Online";
+                    OffOnDrone.ForeColor = Color.Lime;
+                }
+                else
+                {
+                    OffOnDrone.Text = "Offline";
+                    OffOnDrone.ForeColor = Color.Red;
+                }
+                Ping P = new Ping();
+                PingReply Pr = P.Send("192.168.43.30");
+                if (Pr.Status == IPStatus.Success)
+                {
+                    LatencyDrone.Text = Pr.RoundtripTime + " ms";
+                    if (Pr.RoundtripTime < 20)
+                        LatencyDrone.ForeColor = Color.Lime;
+                    else if (Pr.RoundtripTime < 80)
+                        LatencyDrone.ForeColor = Color.Orange;
+                    else
+                        LatencyDrone.ForeColor = Color.Red;
+                }
+                else
+                {
+                    LatencyDrone.Text = "Timed out";
+                    LatencyDrone.ForeColor = Color.Red;
+                }
+                ControlConnection.SendMessage("a");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void UpdateImg()
+        {
+            byte[] copy = ImageByteArray.ToArray();
+
+            using (Image image = Image.FromStream(new MemoryStream(copy)))
+            {
+                Bitmap bp = new Bitmap(image);
+                bp.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                picture.Image = bp;
+            }
+        }
+        public void recieveMessage(UdpClient uc, IPEndPoint end) {          
+            ImageByteArray = uc.Receive(ref end);
+            ImageDownloaded?.Invoke();
+        }
+        
+
+        private void RestartConnectionButton_Click(object sender, EventArgs e)
+        {
+            if (!ControlConnection.Client.Connected)
+            {
+                ControlConnection.StartConnection();
+            }
+        }
+
+        public static class ControlConnection
+        {
+            public static TcpClient Client;
+            public static readonly string Address = "192.168.43.30";
+            public static NetworkStream Stream;
+            public static void StartSocket()
+            {
+                Client = new TcpClient();
+                Client.SendBufferSize = 64;
+            }
+            public static void StartConnection()
+            {
+                Client = new TcpClient();
+                Client.Connect(Address, 50000);
+                Stream = Client.GetStream();
+            }
+            public static void SendMessage(string message)
+            {
+                if (Client.Connected)
+                {
+                    try
+                    {
+                        byte[] bytes = Encoding.ASCII.GetBytes(message);
+                        Stream.Write(bytes, 0, bytes.Length);
+                    }
+                    catch
+                    {
+                        Client.Close();                        
+                    }
+                }
+            }
+        }
+
+        public void ChangeControlScheme(object sender, EventArgs e)
+        {
+
+        }        
+    }
+}
